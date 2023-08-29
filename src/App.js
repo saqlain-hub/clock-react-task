d465191a580d2a99d4df148dfdfebd37a6f554d7
saqlain-hub <saqlainsj12@gmail.com>
2023-08-29 07:44:20 +0500

    clock react app useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1> {ctime} </h1>
+      <button onClick={updateTime} > Get Time</button>
     </div>
   );
 }
